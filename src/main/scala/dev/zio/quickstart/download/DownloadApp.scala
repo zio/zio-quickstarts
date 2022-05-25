6e6869be64a96af894c73fc8a9db72ac2dea5fb5
Milad Khajavi <khajavi@gmail.com>
2022-05-25 18:14:32 +0430

    move files to resources.

--- src/main/scala/dev/zio/quickstart/download/DownloadApp.scala
+++ src/main/scala/dev/zio/quickstart/download/DownloadApp.scala
@@ -1,10 +1,8 @@
 package dev.zio.quickstart.download
 
 import zhttp.http._
+import zio._
 import zio.stream.ZStream
-import zio.{Schedule, ZIO, durationInt}
-
-import java.io.File
 
 // An http app that: 
 //  - Doesn't require any environment
@@ -15,36 +13,25 @@ object DownloadApp {
     Http.collectHttp[Request] {
       // GET /download
       case Method.GET -> !! / "download" =>
-        for {
-          file <- Http.fromZIO(
-            ZIO.attemptBlocking(new File("file.txt"))
+        val fileName = "file.txt"
+        Http.fromStream(ZStream.fromResource(fileName)).setHeaders(
+          Headers(
+            ("Content-Type", "application/octet-stream"),
+            ("Content-Disposition", s"attachment; filename=${fileName}")
           )
-          r <-
-            if (file.exists())
-              Http.response(
-                Response(
-                  Status.Ok,
-                  Headers(
-                    ("Content-Type", "application/octet-stream"),
-                    ("Content-Disposition", s"attachment; filename=${file.getName}")
-                  ),
-                  HttpData.fromFile(file)
-                )
-              )
-            else Http.response(Response.status(Status.NotFound))
-        } yield r
+        )
 
       // Download a large file using streams
       // GET /download/stream
       case Method.GET -> !! / "download" / "stream" =>
-        val file = new File("bigfile.txt")
+        val file = "bigfile.txt"
         Http.fromStream(
-          ZStream.fromFile(file)
+          ZStream.fromResource(file)
             .schedule(Schedule.spaced(50.millis))
         ).setHeaders(
           Headers(
             ("Content-Type", "application/octet-stream"),
-            ("Content-Disposition", s"attachment; filename=${file.getName}")
+            ("Content-Disposition", s"attachment; filename=${file}")
           )
         )
     }
