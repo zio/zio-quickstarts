cbf14a5817aad7c5575d0dc61363f3e6db387ed3
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2023-03-15 11:46:55 +0100

    Changed the Map inside InmemoryUserRepo to an immutable one.
    Refs should contain immutable values.

--- src/main/scala/dev/zio/quickstart/users/InmemoryUserRepo.scala
+++ src/main/scala/dev/zio/quickstart/users/InmemoryUserRepo.scala
@@ -3,22 +3,22 @@ import zio.*
 
 import scala.collection.mutable
 
-  case class InmemoryUserRepo(map: Ref[mutable.Map[String, User]]) extends UserRepo:
-    def register(user: User): UIO[String] =
-      for
-        id <- Random.nextUUID.map(_.toString)
-        _ <- map.updateAndGet(_ addOne(id, user))
-      yield id
+case class InmemoryUserRepo(map: Ref[Map[String, User]]) extends UserRepo:
+  def register(user: User): UIO[String] =
+    for
+      id <- Random.nextUUID.map(_.toString)
+      _ <- map.updateAndGet(_ + (id -> user))
+    yield id
 
-    def lookup(id: String): UIO[Option[User]] =
-      map.get.map(_.get(id))
-     
-    def users: UIO[List[User]] =
-       map.get.map(_.values.toList) 
+  def lookup(id: String): UIO[Option[User]] =
+    map.get.map(_.get(id))
+    
+  def users: UIO[List[User]] =
+      map.get.map(_.values.toList) 
 
-  object InmemoryUserRepo {
-    def layer: ZLayer[Any, Nothing, InmemoryUserRepo] =
-      ZLayer.fromZIO(
-        Ref.make(mutable.Map.empty[String, User]).map(new InmemoryUserRepo(_))
-      )
-  }
+object InmemoryUserRepo {
+  def layer: ZLayer[Any, Nothing, InmemoryUserRepo] =
+    ZLayer.fromZIO(
+      Ref.make(Map.empty[String, User]).map(new InmemoryUserRepo(_))
+    )
+}
