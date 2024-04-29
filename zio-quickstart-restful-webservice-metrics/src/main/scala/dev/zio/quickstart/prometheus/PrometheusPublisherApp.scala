4107ef88e2f815958d6096acee95246ca075f9ef
Milad Khajavi <khajavi@gmail.com>
2024-04-29 10:06:18 +0330

    update webservice-metrics.

--- zio-quickstart-restful-webservice-metrics/src/main/scala/dev/zio/quickstart/prometheus/PrometheusPublisherApp.scala
+++ zio-quickstart-restful-webservice-metrics/src/main/scala/dev/zio/quickstart/prometheus/PrometheusPublisherApp.scala
@@ -5,9 +5,10 @@ import zio.http._
 import zio.metrics.connectors.prometheus.PrometheusPublisher
 
 object PrometheusPublisherApp {
-  def apply(): Http[PrometheusPublisher, Nothing, Request, Response] = {
-    Http.collectZIO[Request] { case Method.GET -> Root / "metrics" =>
-      ZIO.serviceWithZIO[PrometheusPublisher](_.get.map(Response.text))
-    }
-  }
+  def apply(): Routes[PrometheusPublisher, Nothing] =
+    Routes(
+      Method.GET / "metrics" -> handler(
+        ZIO.serviceWithZIO[PrometheusPublisher](_.get.map(Response.text))
+      )
+    )
 }
