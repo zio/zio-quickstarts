53d4f73450deb5a27bb7ec87d7695c4a50f38fbe
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-08-14 13:36:24 +0530

    Update MainApp.scala

--- zio-quickstart-restful-webservice/src/main/scala/dev/zio/quickstart/MainApp.scala
+++ zio-quickstart-restful-webservice/src/main/scala/dev/zio/quickstart/MainApp.scala
@@ -1,3 +1,4 @@
+
 package dev.zio.quickstart
 
 import dev.zio.quickstart.counter.CounterRoutes
@@ -9,44 +10,20 @@ import dev.zio.quickstart.users.{
   UserRoutes
 }
 import zio._
-import zio.Console._
 import zio.http._
-import scala.util.Random
 
 object MainApp extends ZIOAppDefault:
   def run =
-    val retries        = 5
-    val portRangeStart = 8000
-    val portRangeEnd   = 8005
+    Server
+      .serve(
+        GreetingRoutes() ++ DownloadRoutes() ++ CounterRoutes() ++ UserRoutes()
+      )
+      .provide(
+        Server.defaultWithPort(8080),
 
-    def allocatePort(retriesLeft: Int): ZIO[Any, Throwable, Unit] =
-      if (retriesLeft <= 0) {
-        ZIO.fail(
-          new RuntimeException(
-            "Failed to allocate a port within the retry limit"
-          )
-        )
-      } else {
-        val randomPort =
-          Random.nextInt(portRangeEnd - portRangeStart + 1) + portRangeStart
-        Server
-          .serve(
-            GreetingRoutes() ++ DownloadRoutes() ++ CounterRoutes() ++ UserRoutes()
-          )
-          .provide(
-            Server.defaultWithPort(randomPort),
+        // An layer responsible for storing the state of the `counterApp`
+        ZLayer.fromZIO(Ref.make(0)),
 
-            // An layer responsible for storing the state of the `counterApp`
-            ZLayer.fromZIO(Ref.make(0)),
-
-            // To use the persistence layer, provide the `PersistentUserRepo.layer` layer instead
-            InmemoryUserRepo.layer
-          )
-          .catchAll(_ => allocatePort(retriesLeft - 1))
-      }
-
-    allocatePort(retries)
-      .fold(
-        ex => ExitCode.failure,
-        _ => ExitCode.success
+        // To use the persistence layer, provide the `PersistentUserRepo.layer` layer instead
+        InmemoryUserRepo.layer
       )
