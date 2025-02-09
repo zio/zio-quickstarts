a6603945d5dd49715f2f1feb0784888dbe15e9d8
Erik van Oosten <e.vanoosten@grons.nl>
2025-02-09 10:43:28 +0100

    Zio-kafka - Deprecate accessor methods
    Bring the zio-kafka quickstart up to date with:
    - update the docker-compose for Kafka
    - remove usages of accessor methods
    - simplify the examples
    - update dependencies

--- zio-quickstart-kafka/src/main/scala/dev/zio/quickstart/JsonStreamingKafkaApp.scala
+++ zio-quickstart-kafka/src/main/scala/dev/zio/quickstart/JsonStreamingKafkaApp.scala
@@ -11,8 +11,10 @@ import zio.stream.ZStream
 import java.time.OffsetDateTime
 import java.util.UUID
 
+/** This is the data we will be sending to Kafka in JSON format. */
 case class Event(uuid: UUID, timestamp: OffsetDateTime, message: String)
 
+/** A zio-json encoder/decoder for [[Event]]. */
 object Event {
   implicit val encoder: JsonEncoder[Event] =
     DeriveJsonEncoder.gen[Event]
@@ -21,12 +23,10 @@ object Event {
     DeriveJsonDecoder.gen[Event]
 }
 
-object KafkaSerde {
-  val key: Serde[Any, Int] =
-    Serde.int
-
-  val value: Serde[Any, Event] =
-    Serde.string.inmapM[Any, Event](s =>
+/** A zio-kafka serializer/deserializer for [[Event]]. */
+object EventKafkaSerde {
+  val event: Serde[Any, Event] =
+    Serde.string.inmapZIO[Any, Event](s =>
       ZIO
         .fromEither(s.fromJson[Event])
         .mapError(e => new RuntimeException(e))
@@ -34,49 +34,55 @@ object KafkaSerde {
 }
 
 object JsonStreamingKafkaApp extends ZIOAppDefault {
-  private val BOOSTRAP_SERVERS = List("localhost:29092")
+  private val BOOSTRAP_SERVERS = List("localhost:9092")
   private val KAFKA_TOPIC      = "json-streaming-hello"
 
-  private val producer: ZLayer[Any, Throwable, Producer] =
-    ZLayer.scoped(
-      Producer.make(
-        ProducerSettings(BOOSTRAP_SERVERS)
-      )
-    )
+  def run: ZIO[Any, Throwable, Unit] = {
+    val p: ZIO[Any, Throwable, Unit] =
+      ZIO.scoped {
+        for {
+          producer <- Producer.make(ProducerSettings(BOOSTRAP_SERVERS))
+          _ <- ZStream
+            .repeatZIO(Random.nextUUID <*> Clock.currentDateTime)
+            .schedule(Schedule.spaced(1.second))
+            .map { case (uuid, time) =>
+              new ProducerRecord(
+                KAFKA_TOPIC,
+                time.getMinute,
+                Event(uuid, time, "Hello, World!")
+              )
+            }
+            .via(producer.produceAll(Serde.int, EventKafkaSerde.event))
+            .runDrain
+        } yield ()
+      }
 
-  private val consumer: ZLayer[Any, Throwable, Consumer] =
-    ZLayer.scoped(
-      Consumer.make(
-        ConsumerSettings(BOOSTRAP_SERVERS)
-          .withGroupId("streaming-kafka-app")
-      )
-    )
-
-  def run = {
-    val p: ZStream[Producer, Throwable, Nothing] =
-      ZStream
-        .repeatZIO(Random.nextUUID <*> Clock.currentDateTime)
-        .schedule(Schedule.spaced(1.second))
-        .map { case (uuid, time) =>
-          new ProducerRecord(
-            KAFKA_TOPIC,
-            time.getMinute,
-            Event(uuid, time, "Hello, World!")
+    val c: ZIO[Any, Throwable, Unit] =
+      ZIO.scoped {
+        for {
+          consumer <- Consumer.make(
+            ConsumerSettings(BOOSTRAP_SERVERS).withGroupId("streaming-kafka-app")
           )
-        }
-        .via(Producer.produceAll(KafkaSerde.key, KafkaSerde.value))
-        .drain
+          _ <- consumer
+            .plainStream(
+              Subscription.topics(KAFKA_TOPIC),
+              Serde.int,
+              EventKafkaSerde.event
+            )
+            .tap { r =>
+              val event: Event = r.value
+              Console.printLine(
+                s"Event ${event.uuid} was sent at ${event.timestamp} with message ${event.message}"
+              )
+            }
+            .map(_.offset)
+            .aggregateAsync(Consumer.offsetBatches)
+            .mapZIO(_.commit)
+            .runDrain
+        } yield ()
+      }
 
-    val c: ZStream[Consumer, Throwable, Nothing] =
-      Consumer
-        .plainStream(Subscription.topics(KAFKA_TOPIC), Serde.int, Serde.string)
-        .tap(r => Console.printLine(r.value))
-        .map(_.offset)
-        .aggregateAsync(Consumer.offsetBatches)
-        .mapZIO(_.commit)
-        .drain
-
-    (p merge c).runDrain.provide(producer, consumer)
+    p <&> c
   }
 
 }
